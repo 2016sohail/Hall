743481d7d24d82c5217cb8a720611ef1fa7662d1
sohailansari <ansari.sohail2013@gmail.com>
2017-02-28 21:05:47 +0530

    UI changes

---
+++ links.php
@@ -0,0 +1,15 @@
+ <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="all" >
+    <link rel="stylesheet" href="assets/css/min/jqueryui.min.css" media="all" >
+    <link rel="stylesheet" href="vendor/animate-css/animate.css" media="all" >
+    <link rel="stylesheet" href="assets/font/iconfont/iconstyle.css" media="all" >
+    <link rel="stylesheet" href="assets/css/min/main.min.css" media="all" >
+    <link rel="stylesheet" href="css/font-awesome.css" media="all" >
+    <link rel="stylesheet" href="css/mycss.css">
+    <link rel="stylesheet" href="css/hover-min.css">
+
+
+
+
+    <script type="text/javascript" src="vendor/js-cookie/src/js.cookie.js"></script>
